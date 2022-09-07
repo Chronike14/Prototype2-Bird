0edf731a9ffbc9b3e9517851531d34dbf1a0520e
GrrantSmith121 <gss1@iu.edu>
2022-09-06 21:10:16 -0400

    Player can now move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,10 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D rb;
 
+    public float moveSpeed = 5f;
+
+    Vector2 movement;
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,13 +25,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     
-        if (Input.GetKey(KeyCode.W)) 
+        /*
+        if (Input.GetKey(KeyCode.W))
         {
-            rb.velocity = new Vector2(3, rb.velocity.x);
+            rb.transform.Translate(Vector2.right * speed);
         }
 
-        if (Input.GetKey(KeyCode.S)) 
+        if (Input.GetKey(KeyCode.S))
         {
             rb.velocity = new Vector2(-3, rb.velocity.x);
         }
@@ -41,5 +45,14 @@ public class PlayerController : MonoBehaviour
         {
             rb.velocity = new Vector2(3, rb.velocity.y);
         }
+        */
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate() 
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
