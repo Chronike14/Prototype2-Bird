86cb13d3bf90de0e91f6bc2d713334eb176ab746
GrrantSmith121 <gss1@iu.edu>
2022-09-06 22:01:17 -0400

    Implemented horrible random color system CHANGE LATER

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,18 +20,102 @@ public class PlayerController : MonoBehaviour
 
     string colorString;
 
+
+
+    public Sprite One;
     public Sprite Two;
+    public Sprite Three;
+    public Sprite Four;
+    public Sprite Five;
+    public Sprite Six;
+    public Sprite Seven;
+    public Sprite Eight;
+    public Sprite Nine;
+    public Sprite Ten;
+    public Sprite Eleven;
+    public Sprite Twelve;
+    public Sprite Thirteen;
+    public Sprite Fourteen;
+    public Sprite Fivteen;
+    public Sprite Sixteen;
 
     // Start is called before the first frame update
     void Start()
     {
 
         rb = gameObject.GetComponent<Rigidbody2D>();
-        colorNum = randColor.Next();
-        colorString = "" + colorNum;
-        this.gameObject.GetComponent<SpriteRenderer>().sprite = Two;
+        RandColor();
 
 
+    }
+
+    void RandColor() 
+    {
+        colorNum = randColor.Next(1, 17);
+        if (colorNum == 1)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = One;
+        }
+        if (colorNum == 2)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Two;
+        }
+        if (colorNum == 3)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Three;
+        }
+        if (colorNum == 4)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Four;
+        }
+        if (colorNum == 5)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Five;
+        }
+        if (colorNum == 6)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Six;
+        }
+        if (colorNum == 7)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Seven;
+        }
+        if (colorNum == 8)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Eight;
+        }
+        if (colorNum == 9)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Nine;
+        }
+        if (colorNum == 10)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Ten;
+        }
+        if (colorNum == 11)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Eleven;
+        }
+        if (colorNum == 12)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Twelve;
+        }
+        if (colorNum == 13)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Thirteen;
+        }
+        if (colorNum == 14)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Fourteen;
+        }
+        if (colorNum == 15)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Fivteen;
+        }
+        if (colorNum == 16)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Sixteen;
+        }
     }
 
     // Update is called once per frame
